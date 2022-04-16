ba3627ded6553a875d0c08076d3cc91df8aa559c
Provashish Roy <provashishroy95@gmail.com>
2022-04-16 17:18:17 +0600

    create user successfully

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,7 +1,43 @@
 import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
     const [login, setLogin] = useState(false);
+    const [confirmError, setConfirmError] = useState('');
+
+    const [userInfo, setUserInfo] = useState({
+        email: '',
+        password: '',
+        confirmPass: ''
+    });
+
+    //for user create with email and password ....
+    const [
+        createUserWithEmailAndPassword,
+        createUser,
+        createLoading,
+        createError,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleFormInput = event => {
+        userInfo[event.target.name] = event.target.value;
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (userInfo.password !== userInfo.confirmPass) {
+            setConfirmError('Password can not match!');
+            return;
+        }
+
+        if (!login) {
+            setConfirmError('');
+            createUserWithEmailAndPassword(userInfo.email, userInfo.password);
+        }
+
+    }
+
     return (
         <div className='container'>
             <div className="row mt-5">
@@ -9,21 +45,21 @@ const Login = () => {
                     <h2 className='text-center text-info'>
                         {login ? 'Login' : 'Register'}
                     </h2>
-                    <form className='w-50 mx-auto'>
+                    <form onSubmit={handleSubmit} className='w-50 mx-auto'>
                         <div className="mb-3">
                             <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            <input onBlur={(event) => handleFormInput(event)} type="text" name='email' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                            <label htmlFor="cExampleInputPassword1" className="form-label">Password</label>
+                            <input onBlur={(event) => handleFormInput(event)} type="password" name='password' className="form-control" id="CexampleInputPassword1" />
                         </div>
                         {
                             !login ?
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputConfirmPassword1" className="form-label">Confirm Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                                    <input onBlur={(event) => handleFormInput(event)} type="password" name='confirmPass' className="form-control" id="exampleInputPassword1" />
                                 </div>
                                 :
                                 ''
@@ -37,6 +73,13 @@ const Login = () => {
                                 login ? 'Login' : 'Register'
                             }
                         </button>
+                        <p className='text-danger'>{confirmError}</p>
+                        {
+                            createError && <p className='text-danger'>{createError.message}</p>
+                        }
+                        {
+                            createUser && <p className='text-success'>User Create Successfully</p>
+                        }
                     </form>
                 </div>
             </div>
