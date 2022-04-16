e3f9971e467c27bbea481d98c16deec1e7de673b
Provashish Roy <provashishroy95@gmail.com>
2022-04-16 13:29:57 +0600

    added custom link on navbar

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CustomLink from '../../CustomLink/CustomLink';
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Random User</Link>
+            <div className="container">
+                <CustomLink className="navbar-brand" to="/">Random User</CustomLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link " aria-current="page" to="/home">Home</Link>
+                            <CustomLink className="nav-link " aria-current="page" to="/home">Home</CustomLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/users">Users</Link>
+                            <CustomLink className="nav-link" to="/users">Users</CustomLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
+                            <CustomLink className="nav-link" to="/login">Login</CustomLink>
                         </li>
                     </ul>
+                    <span className='navbar-text'>User</span>
                 </div>
             </div>
         </nav>
